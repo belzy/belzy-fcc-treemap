0dab146ad6c066b4ff022de9f6aee175e4609864
brandon-elzy <brandon.g.elzy@line-zero.com>
2020-07-04 14:17:25 -0500

    Completed Project

--- client/js/main.js
+++ client/js/main.js
@@ -42,10 +42,9 @@
         const platformArr = data['children'].map(platform => platform['name']);
         const colorArr = ['#4C92C3', '#FFC993', '#DE5253', '#D1C0DD', '#E992CE', '#D2D2D2', '#BED2ED', '#56B356', '#FFADAB', '#A3786F', '#F9C5DB', '#C9CA4E', '#FF993E', '#ADE5A1', '#A985CA', '#D0B0A9', '#999999', '#E2E2A4'];
 
+
         const w = 960;
         const h = 570;
-        // const w = 1200;
-        // const h = 600;
         const padding = 2;
 
         // Color scale.
@@ -61,15 +60,15 @@
 
         const svg = d3.select('svg')
             .attr('width', w)
-            .attr('height', h)
+            .attr('height', h + 400)
 
         const node = svg.selectAll('g')
             .data(treemap.leaves())
             .enter()
             .append('g')
             .attr('transform', d => `translate(${ d.x0 }, ${ d.y0 })`)
-
-        node.append('rect')
+            
+            node.append('rect')
             .attr('class', 'tile')
             .attr('data-name', d => d['data']['name'])
             .attr('data-category', d => d['data']['category'])
@@ -77,6 +76,39 @@
             .attr('fill', d => colorScale(d['data']['category']))
             .attr('width', d => d.x1 - d.x0)
             .attr('height', d => d.y1 - d.y0)
+            .attr('data-area-number', (d, i) => i)
+            .on('mouseenter', (d, i) => {
+
+                const name = d['data']['name'];
+                const category = d['data']['category'];
+                const value = d['data']['value'];
+
+                 // rect.top, rect.right, rect.bottom, rect.left
+                 const cellRect = document.querySelector(`[data-area-number="${i}"]`).getBoundingClientRect();
+
+                 const topOffset = -15;
+                 const leftOffset = 10;
+
+                d3.select('#tooltip')
+                        .attr('data-value', () => value)
+                        .style('top', `${cellRect.top + ((cellRect.bottom - cellRect.top) / 2) + topOffset}px`)
+                        .style('left', `${cellRect.right + leftOffset}px`)
+                        .style('opacity', '0.7')
+
+                d3.select('#tooltip-name')
+                    .text(`Name: ${name}`)
+
+                d3.select('#tooltip-category')
+                    .text(`Category: ${category}`)
+
+                d3.select('#tooltip-value')
+                    .text(`Value: ${value}`)
+
+
+            }).on('mouseout', () => {
+                d3.select('#tooltip')
+                    .style('opacity', '0.0')
+            })
 
         node.append('text')
             .selectAll('tspan')
@@ -86,7 +118,77 @@
             .attr('x', 4)
             .attr('y', (d, i) => 14 + (i * 12))
             .text(d => d)
-   
+
+        // Legend
+        let size = 30;
+        let horizSpacing = 100;
+        let vertSpacing = 12;
+        let x = 0;
+        let y = 0;
+
+        const legend = svg.append('g')
+            .attr('id', 'legend')
+            .attr('transform', 'translate(350, 650)')
+
+
+        const textNode = legend.selectAll('g')
+            .data(platformArr)
+            .enter()
+            .append('g')
+
+        textNode.append('rect')
+            .attr('class', 'legend-item')
+            .attr('fill', d => colorScale(d))
+            .attr('width', size)
+            .attr('height', size)
+            .attr('x', (d, i) => {
+                if (i % 3 === 1) {
+                    x += size + horizSpacing;
+                    return x;
+                }
+                else if (i % 3 === 2) {
+                    x += size + horizSpacing;
+                    return x;
+                }
+                else if (i % 3 === 0) {
+                    x = 0;
+                    return x;
+                }
+            })
+            .attr('y', (d, i) => {
+                if (i > 0 && i % 3 === 0) {
+                    y += size + vertSpacing;
+                    return y;
+                } else return y;
+            })
+
+        const textHorizSpace = 6;
+        x = size + textHorizSpace;
+        y = size / 2 + 5;
+        textNode
+            .append('text')
+            .attr('font-size', '15px')
+            .attr('x', (d, i) => {
+                if (i % 3 === 1) {
+                    x += size + horizSpacing;
+                    return x;
+                }
+                else if (i % 3 === 2) {
+                    x += size + horizSpacing;
+                    return x;
+                }
+                else if (i % 3 === 0) {
+                    x = size + textHorizSpace;
+                    return x;
+                }
+            })
+            .attr('y', (d, i) => {
+                if (i > 0 && i % 3 === 0) {
+                    y += size + vertSpacing;
+                    return y;
+                } else return y;
+            })
+            .text(d => d)
     });
 
 })();
