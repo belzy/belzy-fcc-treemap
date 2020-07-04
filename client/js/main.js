ccb82f6219c1efad27240d988f425cf9db476bb9
brandon-elzy <brandon.g.elzy@line-zero.com>
2020-07-04 13:04:18 -0500

    Added text to cells

--- client/js/main.js
+++ client/js/main.js
@@ -40,10 +40,12 @@
     getData(data => {
 
         const platformArr = data['children'].map(platform => platform['name']);
-        const colorArr = ['#4C92C3', '#FFC993', '#DE5253', '#D1C0DD', '#E992CE', '#D2D2D2', '#D2D2D2', '#56B356', '#FFADAB', '#A3786F', '#F9C5DB', '#C9CA4E', '#FF993E', '#ADE5A1', '#A985CA', '#D0B0A9', '#999999', '#E2E2A4'];
+        const colorArr = ['#4C92C3', '#FFC993', '#DE5253', '#D1C0DD', '#E992CE', '#D2D2D2', '#BED2ED', '#56B356', '#FFADAB', '#A3786F', '#F9C5DB', '#C9CA4E', '#FF993E', '#ADE5A1', '#A985CA', '#D0B0A9', '#999999', '#E2E2A4'];
 
-        const w = 1200;
-        const h = 600;
+        const w = 960;
+        const h = 570;
+        // const w = 1200;
+        // const h = 600;
         const padding = 2;
 
         // Color scale.
@@ -57,20 +59,34 @@
         const treemap = d3.treemap()
             .size([w, h])(root);
 
-        const svg = d3.select('#svg-treemap')
+        const svg = d3.select('svg')
             .attr('width', w)
             .attr('height', h)
-            .selectAll('rect')
+
+        const node = svg.selectAll('g')
             .data(treemap.leaves())
             .enter()
-            .append('rect')
-            .attr('x', d => d.x0)
-            .attr('y', d => d.y0)
-            .attr('width', d => (d.x1 - d.x0))
-            .attr('height', d => (d.y1 - d.y0))
-            .attr('stroke', '#FFFFFF')
-            .attr('fill', d => colorScale(d['data']['category']))
+            .append('g')
+            .attr('transform', d => `translate(${ d.x0 }, ${ d.y0 })`)
 
+        node.append('rect')
+            .attr('class', 'tile')
+            .attr('data-name', d => d['data']['name'])
+            .attr('data-category', d => d['data']['category'])
+            .attr('data-value', d => d['data']['value'])
+            .attr('fill', d => colorScale(d['data']['category']))
+            .attr('width', d => d.x1 - d.x0)
+            .attr('height', d => d.y1 - d.y0)
+
+        node.append('text')
+            .selectAll('tspan')
+            .data(d => d['data']['name'].split(' '))
+            .enter()
+            .append('tspan')
+            .attr('x', 4)
+            .attr('y', (d, i) => 14 + (i * 12))
+            .text(d => d)
+   
     });
 
 })();
